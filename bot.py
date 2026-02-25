ad39e8f58cb478f0adebacf021b2d06c3afbfacf
strahinjapavlovic780-png <strahinjapavlovic780@gmail.com>
2026-02-25 20:44:07 +0100

    Add message content intent to bot configuration

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 from discord import app_commands
 import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.command()
 async def mmpanel(ctx):
     embed = discord.Embed(
